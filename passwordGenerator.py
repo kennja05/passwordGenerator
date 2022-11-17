3637ee9fdc4c5b9650428d7544dcddd3df0c9605
Jacob Kenny <jacobkenny05@gmail.com>
2022-11-16 23:36:40 -0500

    add special chars array function

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -5,20 +5,39 @@
 #    if minLength < 8:
 #        print('Password must be at least 8 characters long')
 
+from codecs import encode
+import io
 import requests
+from random import randint
 
+#get list of words if user does not specify a word or phrase 
 word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 WORDS = response.content.splitlines()
-goodwords = list(filter(lambda word: len(word) > 4, WORDS))
-print([1000].decode('utf-8'))
+good_words = list(filter(lambda word: len(word) > 4, WORDS))
 
-specificWordAffirmation = input("Is there a specific word or phrase you would like to include in your password? y/n: ").lower()
-if specificWordAffirmation == 'y':
+specific_word_affirmation = input("Is there a specific word or phrase you would like to include in your password? y/n: ").lower()
+if specific_word_affirmation == 'y':
     myWord = input("Enter your word or phrase here: ")
 else:
-    print('ok ')
+    myWord = good_words[randint(0, len(good_words))].decode('utf-8')
+
+include_spec_char = input("Would you like to include special characters? y/n: ").lower()
+include_num = input("Would you like to include numbers? y/n: ").lower()
 
 
-includeSpecChar = input("Would you like to include special characters? y/n: ").lower()
-includeNum = input("Would you like to include numbers? y/n: ").lower()
+def special_characters_array():
+    char_codes = []
+    i = 33
+    while i <= 48:
+        char_codes.append(i)
+        i+=1
+    i = 58
+    while i <= 64:
+        char_codes.append(i)
+        i+=1
+    i = 91
+    while i <= 96:
+        char_codes.append(i)
+        i+=1
+    return char_codes

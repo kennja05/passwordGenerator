ca06be5c1fcc5515f346dd1527798b2b419644db
Jacob Kenny <jacobkenny05@gmail.com>
2022-11-22 20:30:07 -0500

    remove commented out section

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,12 +1,3 @@
-#minLength = 0
-#set min length. generated password may exceed this
-#while minLength < 8:
-#    minLength = int(input("What is your desired password length? "))
-#    if minLength < 8:
-#        print('Password must be at least 8 characters long')
-
-from codecs import encode
-import io
 import requests
 from random import randint
 
@@ -15,7 +6,6 @@ word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 WORDS = response.content.splitlines()
 good_words = list(filter(lambda word: len(word) > 4, WORDS))
-
 specific_word_affirmation = input("Is there a specific word or phrase you would like to include in your password? y/n: ").lower()
 if specific_word_affirmation == 'y':
     myWord = input("Enter your word or phrase here: ")

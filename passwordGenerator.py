9097ef2df748867b934bb210efb49819691bb713
Jacob Kenny <jacobkenny05@gmail.com>
2022-11-28 22:24:37 -0500

    creates password for no spec chars or numbers

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,5 +1,9 @@
+from xml.etree.ElementInclude import include
 import requests
 from random import randint
+
+def random_word():
+    print('done')
 
 def special_characters_array():
     char_codes = []
@@ -25,12 +29,14 @@ WORDS = response.content.splitlines()
 good_words = list(filter(lambda word: len(word) > 4, WORDS))
 specific_word_affirmation = input("Is there a specific word or phrase you would like to include in your password? y/n: ").lower()
 if specific_word_affirmation == 'y':
-    myWord = input("Enter your word or phrase here: ")
+    my_word = input("Enter your word or phrase here: ")
 else:
-    myWord = good_words[randint(0, len(good_words))].decode('utf-8')
+    my_word = good_words[randint(0, len(good_words))].decode('utf-8')
 
 include_spec_char = input("Would you like to include special characters? y/n: ").lower()
+include_spec_char = True if include_spec_char == 'y' else False
 include_num = input("Would you like to include numbers? y/n: ").lower()
+include_num = True if include_num == 'y' else False
 min_length = 0
 while min_length <= 6 or min_length > 30:
     desired_length = input("What is the minimum length for your password?: ")
@@ -41,5 +47,9 @@ while min_length <= 6 or min_length > 30:
     if int(desired_length) <= 6 or int(desired_length) > 100:
         print("Miminum length must be an integer of at least 6 and less than 100")
     min_length = int(desired_length)
-print(special_characters_array())
 
+password = my_word
+if not include_spec_char and not include_num:
+    while len(password) < min_length:
+        password = password + good_words[randint(0, len(good_words))].decode('utf-8')
+print(password)
